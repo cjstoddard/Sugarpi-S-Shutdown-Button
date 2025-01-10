f7ab381ff786dcb84db7a616191af41a3247602e
cjstoddard <chris.j.stoddard@gmail.com>
2025-01-09 20:45:54 -0600

    modified:   shutdown.sh

--- shutdown.sh
+++ shutdown.sh
@@ -6,3 +6,9 @@ if [ "$(pinctrl lev 3)" = "0" ]; then
   sudo shutdown now
 fi
 done
+
+## If you are using a legacy version of Raspary Pi OS that does not support pinctrl,
+## run "sudo apt install raspi-gpio" and replace 
+##   if [ "$(pinctrl lev 3)" = "0" ]; then
+## with
+##   if [ "$(raspi-gpio get 3 | awk -v RS=" " -F "=" -v k="level" '$1==k {print $2}')" = "0" ]; then
